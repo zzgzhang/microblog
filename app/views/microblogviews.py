daf1e02d83090559539a25dfe363d17c6c4741e2
zhenyuanwei <zhenyuanwei@foxmail.com>
2017-08-06 17:22:54 +0800

    Hello World完成

--- app/views/microblogviews.py
+++ app/views/microblogviews.py
@@ -1,9 +1,6 @@
 from app import app
-from flask import render_template
-from flask import request
-from flask import session
 
 @app.route('/')
-def hello_world():
+def index():
     return 'Hello World!'
 
