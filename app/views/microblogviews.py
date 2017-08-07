a4df06088381ad038e71b495202bc17926645355
zhenyuanwei <zhenyuanwei@foxmail.com>
2017-08-07 20:21:51 +0800

    模版使用完成

--- app/views/microblogviews.py
+++ app/views/microblogviews.py
@@ -1,6 +1,39 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 def index():
+    user = {'nickname': 'Miguel'}  # fake user
+    template_naeme = 'index.html'
+    return render_template(template_naeme, title='Home', user=user)
+
+@app.route('/hello')
+def hello():
     return 'Hello World!'
 
+
+@app.route('/html')
+def html():
+    user = {'nickname': 'Miguel'}  # fake user
+    return '''
+    <html>
+      <head>
+        <title>Home Page</title>
+      </head>
+      <body>
+        <h1>Hello, ''' + user['nickname'] + '''</h1>
+      </body>
+    </html>
+    '''
+
+@app.route('/posts')
+def posts():
+    # fake user
+    user = {'nickname': 'Miguel'}
+
+    # fake array of posts
+    posts = [{'author': {'nickname': 'John'}, 'body': 'Beautiful day in Portland!'},
+             {'author': {'nickname': 'Susan'}, 'body': 'The Avengers movie was so cool!'}]
+
+    template_naeme = 'posts.html'
+    return render_template(template_naeme, title='Posts', posts=posts, user=user)
