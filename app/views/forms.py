10cc89232aac1dacb7f1813c0a6691253973c0eb
zhenyuanwei <zhenyuanwei@foxmail.com>
2017-08-08 22:42:06 +0800

    web表单完成

--- app/views/forms.py
+++ app/views/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import Form
-from wtforms import StringField, BooleanField
+from wtforms import StringField, PasswordField
 from wtforms.validators import DataRequired
 
 class LoginForm(Form):
-    openid = StringField('openid', validators=[DataRequired()])
-    remember_me = BooleanField('remember_me', default=False)
+    username = StringField(label='Username', validators=[DataRequired()])
+    password = PasswordField(label='Password', validators=[DataRequired()])
