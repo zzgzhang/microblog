c26ecb3c3504d91ff090ddb3946a55470cef2a0b
weizy1981 <zhenyuanwei@foxmail.com>
2017-08-09 18:34:41 +0800

    init comments

--- app/config.py
+++ app/config.py
@@ -4,7 +4,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # 数据库链接设置
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'microblog.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
-SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 CSRF_ENABLED = True
 # SECRET_KEY设置当CSRF启用时有效，这将生成一个加密的token供表单验证使用，你要确保这个KEY足够复杂不会被简单推测。
