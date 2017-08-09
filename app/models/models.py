be38806224ec9a650fff5294c29c395b36d328d3
weizy1981 <zhenyuanwei@foxmail.com>
2017-08-09 19:03:57 +0800

    init comments

--- app/models/models.py
+++ app/models/models.py
@@ -6,13 +6,21 @@ Base = declarative_base()
 class Users(Base):
     __tablename__ = 'users'
 
-    id = sa.Column(sa.Integer, primary_key = True)
+    id = sa.Column(sa.Integer, primary_key=True)
     username = sa.Column(sa.String(20), nullable=False)
     password = sa.Column(sa.String(20), nullable=False)
     nickname = sa.Column(sa.String(64), nullable=False)
     email = sa.Column(sa.String(120), nullable=False)
     # 增加一个新字段description
     description = sa.Column(sa.String(200), nullable=False)
+
+class Posts(Base):
+    __tablename__ = 'posts'
+
+    id = sa.Column(sa.Integer, primary_key=True)
+    body = sa.Column(sa.String(500), nullable=True)
+    timestamp = sa.Column(sa.Time(), nullable=True)
+    user_id = sa.Column(sa.Integer(), nullable=True)
 
 if __name__ == "__main__":
     engine = sa.create_engine("sqlite:////Users/wzy/Documents/PycharmProjects/microblog/app/microblog.db")
